c5d2dd0cd0f03b8df147ccbb01fc76beebf0e520
Orhan Türk <orhan.turk21@gmail.com>
2016-09-20 16:41:48 +0300

    Add files via upload

---
+++ SerialCounter/SerialCounter.ino
@@ -0,0 +1,17 @@
+const int ledPin = 13;
+int say = 0;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+void loop()
+{
+  if (say == 15)
+  {
+    digitalWrite(ledPin, HIGH); // ledi yak
+    delay(500);
+    Serial.write(say);
+  }
+  say = say + 1;
+}
