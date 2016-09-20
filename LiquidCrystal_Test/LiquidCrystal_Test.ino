807f481f46855da2be6149f1274559d80731ef2a
Orhan Türk <orhan.turk21@gmail.com>
2016-09-20 16:41:55 +0300

    Add files via upload

---
+++ LiquidCrystal_Test/LiquidCrystal_Test.ino
@@ -0,0 +1,41 @@
+#include <FastIO.h>
+#include <I2CIO.h>
+#include <LCD.h>
+#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C_ByVac.h>
+#include <LiquidCrystal_SI2C.h>
+#include <LiquidCrystal_SR.h>
+#include <LiquidCrystal_SR1W.h>
+#include <LiquidCrystal_SR2W.h>
+#include <LiquidCrystal_SR3W.h>
+#include <SI2CIO.h>
+#include <SoftI2CMaster.h>
+
+
+#include <Wire.h>
+
+#define BACKLIGHT_PIN     13
+LiquidCrystal_I2C lcd(0x27 , 16, 2);
+
+void setup()
+{
+
+  lcd.begin();
+  lcd.backlight();
+
+  // Switch on the backlight
+  pinMode ( BACKLIGHT_PIN, OUTPUT );
+
+  digitalWrite ( BACKLIGHT_PIN, HIGH );
+
+  // Ekrana yazdırılacak metin
+  lcd.print("Proje Hocam");
+
+
+}
+
+void loop()
+{
+
+}
